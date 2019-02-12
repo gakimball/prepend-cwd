6086b04cc30f0c86846a3c7d4e36b42a0e3acd1e
Nicola <nicola.dalmaso@outlook.com>
2019-02-12 20:58:02 +0100

    General updates.
    - Drop Node@4 and Node@6
    - Upgrade xo to latest version
    - Lint fixes after xo upgrade
    - TypeScript definition file test now runs on "npm test"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 /**
  * Prepend the current working directory to a path, unless the path is absolute, in which case return it as-is.
- * @param {String} path - Path to prepend to.
+ * @param {String} p - Path to prepend to.
  * @returns {String} Absolute path.
  */
 module.exports = p => {
--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 'use strict';
 
 const path = require('path');
-const expect = require('chai').expect;
+const {expect} = require('chai');
 const cwd = require('.');
 
 describe('cwd()', () => {
